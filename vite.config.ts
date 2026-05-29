84b0aeccfbdd5e80719eaf856599306d29f61e63
dasarijashwanth <dasarijashwanth@users.noreply.github.com>
2026-05-29 19:05:49 -0400

    Configure base path to root for Netlify Drop

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Profile-Card/',
+  base: '/',
   plugins: [react(), tailwindcss()],
 })
